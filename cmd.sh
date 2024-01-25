b8630c209b27d67bbd5d1df80638e984e0953de4
dmitry-novozhilov <Dmitry@Novozhilov.ru>
2024-01-25 17:01:19 +0300

    coverage badge

--- cmd.sh
+++ cmd.sh
@@ -6,7 +6,7 @@ if [ "$1" == "test" ]; then
 fi
 
 if [ "$1" == "test-n-cover" ]; then
-    go test -coverprofile=/tmp/coverage.out ./... | perl -MFile::Slurp=edit_file -nWE 'print $_; next unless /coverage: ([0-9\.]+)%/; my$cov=$1; my$clr=$cov>80?"green":$cov>50?"yellow":"red"; edit_file {s/(\!\[\]\(https:\/\/img\.shields\.io\/static\/v1\?label=Coverage)&message=[0-9\.]+%&color=[a-z]+\)/$1&message=$cov%&color=$clr)/} "README.md"' 2>/dev/null
+    go test -coverprofile=/tmp/coverage.out ./... | perl -MFile::Slurp=edit_file -nWE 'print $_; next unless /coverage: ([0-9\.]+)%/; my$cov=$1; my$clr=$cov>70?"green":$cov>50?"yellow":"red"; edit_file {s/(\!\[\]\(https:\/\/img\.shields\.io\/static\/v1\?label=Coverage)&message=[0-9\.]+%&color=[a-z]+\)/$1&message=$cov%&color=$clr)/} "README.md"' 2>/dev/null
     exit 0
 fi
 
