f24264a2cebf413909725ae12d8f2e96d56e49c3
dmitry-solta <dmitry.n@solta.io>
2025-11-25 23:47:40 +0300

    fix when field haven't tag

--- parser.go
+++ parser.go
@@ -207,11 +207,8 @@ func (sm structsMap) procType(f *ast.Field, e ast.Expr, ent *entity) {
 			procFieldTag(f, ent, fld)
 
 			if st, ok := sm[id.Name]; ok {
-				for i, f := range st.structType.Fields.List {
-					if f == nil {
-						log.Fatalf("fld.GoName %s; structType %v fields %v field #%d is nil", fld.GoName, st.structType, st.structType.Fields, i)
-					}
-					if newTag(f.Tag.Value, "json") != nil {
+				for _, f := range st.structType.Fields.List {
+					if f.Tag != nil && newTag(f.Tag.Value, "json") != nil {
 						fld.IsJson = true
 					}
 				}
